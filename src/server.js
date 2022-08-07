d3b127f57d19faf7f3c920a7aa7b37d32462dc1f
Eduardo Silva <97840495+eduardosdl@users.noreply.github.com>
2022-08-06 21:44:22 -0300

    server config initial

---
+++ src/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const category = require('./routes/category');
+
+// config
+// express
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+// mongoose
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/biblio').then(() => {
+    console.log('Banco de dados conectado com sucesso');
+}).catch((err) => console.log(`falha ao se conectar: ${err}`));
+
+// rotas
+app.use('/category', category);
+
+const port = 3000;
+app.listen(port, () => console.log(`Server rodando em http://localhost:${port}`));
