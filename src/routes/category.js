d85adcc6293aba1e2fd04b9cd6df8c3b860c6b98
Eduardo Silva <edu.silva.e08@gmail.com>
2022-08-25 10:21:05 -0300

    route to edit category

--- src/routes/category.js
+++ src/routes/category.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const categoryController = require('../controllers/CategoryController');
+const { userConnected } = require('../helpers/userConnected');
+const { adminConnected } = require('../helpers/adminConected');
 
 router.get('/', categoryController.getCategory);
 
 router.post('/new', categoryController.createCategory);
+
+router.put('/edit/:id', categoryController.editCategory);
 
 router.delete('/del/:id', categoryController.deleteCategory);
 
