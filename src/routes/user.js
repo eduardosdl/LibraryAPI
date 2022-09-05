951a4d0db94a8651376fbf8b265718a0375e756d
Eduardo Silva <edu.silva.e08@gmail.com>
2022-09-05 08:58:45 -0300

    add private routes

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/UserController');
+const { userConnected } = require('../helpers/userConnected');
+const { adminConnected } = require('../helpers/adminConected');
 
 router.post('/new', controller.newUser);
 
 router.get('/login', controller.loginUser);
 
-router.patch('/edit/:id', controller.editUser);
+router.patch('/edit/:id', userConnected, controller.editUser);
 
-router.patch('/edit/pass', controller.editUserPass);
+router.patch('/edit/pass', userConnected, controller.editUserPass);
 
-router.patch('/makeadm/:id', controller.makeAdmin);
+router.patch('/makeadm/:id', adminConnected, controller.makeAdmin);
 
-router.delete('/del/:id', controller.deleteUser);
+router.delete('/del/:id', userConnected, controller.deleteUser);
 
 module.exports = router;
