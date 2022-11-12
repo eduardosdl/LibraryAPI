7b6b42c921ea1fd6853d19fc015f5a9b8f9a93bb
eduardosdl <97840495+eduardosdl@users.noreply.github.com>
2022-11-12 09:34:26 -0300

    update to improve reading

--- src/server.js
+++ src/server.js
@@ -1,38 +1,26 @@
 const express = require('express');
-const app = express();
 const mongoose = require('mongoose');
-const cors = require('cors');
-const user = require('./routes/user');
-const book = require('./routes/book');
-const category = require('./routes/category');
+
+const cors = require('./middlewares/cors');
+const router = require('./router');
+
+const app = express();
 
 // config
 const port = process.env.PORT || 3000;
-// cors
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-	res.header(
-		'Access-Control-Allow-Header',
-		'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-	);
-    res.header("Access-Control-Allow-Methods", 'GET,PATCH,PUT,POST,DELETE');
-    app.use(cors());
-    next();
-});
 // express
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
+app.use(cors);
 // mongoose
 mongoose.Promise = global.Promise;
-mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}:27017/library?authSource=admin`, {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@localhost:27017/library?authSource=admin`, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
 }).then(() => {
-	console.log('Banco de dados conectado com sucesso');
-	app.listen(port, () => console.log(`Server rodando em http://localhost:${port}`));
-}).catch((err) => console.log(`falha ao se conectar: ${err}`));
-
+  console.log('conectou no banco');
+}).catch((err) => console.log(`err${err}`));
 // routes
-app.use('/user', user);
-app.use('/book', book);
-app.use('/category', category);
+app.use(router);
+
+app.listen(port, () => console.log(`Server rodando em http://localhost:${port}`));
