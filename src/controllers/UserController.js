1a54cf444a8ad5c613a970e01a443943d08ac2ba
eduardosdl <97840495+eduardosdl@users.noreply.github.com>
2022-11-14 10:34:23 -0300

    remove handle errors and code optimization

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,279 +1,161 @@
-const mongoose = require('mongoose');
-require('../models/User');
-const User = mongoose.model('users');
 const bcrypt = require('bcrypt');
+
 const jwt = require('../helpers/jwt');
+const User = require('../models/User');
 
-const newUser = async (req, res) => {
-    const { name, email, password, confirmPassword } = req.body;
+const createUser = async (req, res) => {
+  const {
+    name, email, password, confirmPassword,
+  } = req.body;
 
-    if(!name) {
-        return res.status(422).send({
-            msg: "O nome é obrigatorio"
-        });
-    }
-
-    if(!email) {
-        return res.status(422).send({
-            msg: "O email é obrigatório"
-        });
-    }
-
-    if(!password) {
-        return res.status(422).send({
-            msg: "A senha é obrigatório"
-        });
-    }
-    
-    if(password != confirmPassword) {
-        return res.status(422).send({
-            msg: "As senhas não condizem"
-        });
-    }
-
-    const userExists = await User.findOne({ email });
-    
-    if(userExists) {
-        return res.status(422).send({
-            msg: "Já exites um usuário com esse email"
-        });
-    }
-
-    const salt = await bcrypt.genSalt(12);
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    const user = new User({
-        name,
-        email,
-        password: passwordHash
+  if (!name || !email || !password || !confirmPassword) {
+    return res.status(422).send({
+      error: 'All fields are required',
     });
+  }
 
-    try {
-        await user.save();
-        const token = jwt.sign({ id: user._id, isAdmin: user.admin });
+  if (password !== confirmPassword) {
+    return res.status(400).send({
+      error: 'Passwords do not match',
+    });
+  }
 
-        res.status(201).send({
-            msg: "Usuário criado e logado com sucesso",
-            data: {
-                id: user._id,
-                name: user.name,
-                email: user.email,
-            },
-            token
-        });
-    } catch (err){
-        console.log('erro: '+err);
-    
-        res.status(500).send({
-            msg: "Houve um erro no servidor, tente novamente mais tarde"
-        });
-    }
-}
+  const salt = await bcrypt.genSalt(12);
+  const hash = await bcrypt.hash(password, salt);
+
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    return res.status(400).send({
+      error: 'This e-mail is already in use',
+    });
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password: hash,
+  });
+
+  const token = jwt.sign({ id: user._id, isAdmin: user.admin });
+
+  res.status(201).send({
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    token,
+  });
+};
 
 const loginUser = async (req, res) => {
-    const [ , hash ] = req.headers.authorization.split(' ');
-    const [email, password] = Buffer.from(hash, 'base64').toString().split(':');
+  const [, hash] = req.headers.authorization.split(' ');
+  const [email, password] = Buffer.from(hash, 'base64').toString().split(':');
 
-    if(!email) {
-        return res.status(422).send({
-            msg: "O email é obrigatório"
-        });
-    }
+  if (!email || !password) {
+    return res.status(400).send({
+      error: 'All fields are required',
+    });
+  }
 
-    if(!password) {
-        return res.status(422).send({
-            msg: "A senha é obrigatório"
-        });
-    }
+  const user = await User.findOne({ email }).select('+password');
 
-    const user = await User.findOne({ email });
-    
-    if(!user) {
-        return res.status(404).send({
-            msg: "Usuário não encontrado"
-        });
-    }
+  if (!user) {
+    return res.status(404).send({
+      error: 'User not found',
+    });
+  }
 
-    const checkPassword = await bcrypt.compare(password, user.password);
+  const checkPassword = await bcrypt.compare(password, user.password);
 
-    if(!checkPassword) {
-        return res.status(401).send({
-            msg: "Senha incorreta"
-        });
-    }
+  if (!checkPassword) {
+    return res.status(401).send({
+      error: 'Password invalid',
+    });
+  }
 
-    try {
-        const token = jwt.sign({ id: user._id, isAdmin: user.admin });
+  const token = jwt.sign({ id: user._id, isAdmin: user.admin });
 
-        res.status(200).send({
-            msg: "Logado com sucesso",
-            data: {
-                id: user._id,
-                name: user.name,
-                email: user.email
-            },
-            token
-        });
-    } catch (err) {
-        console.log('erro: '+err);
-    
-        res.status(500).send({
-            msg: "Houve um erro no servidor, tente novamente mais tarde"
-        });
-    }
-}
+  res.status(200).send({
+    id: user._id,
+    name: user.name,
+    email: user.email,
+    token,
+  });
+};
 
 const editUser = async (req, res) => {
-    const { id } = req.params;
-    const { email, password, name } = req.body;
+  const { id } = req.credentials;
+  const { name, email } = req.body;
 
-    const user = await User.findById(id);
-    const userExists = await User.findOne({ email });
+  // const user = await User.findById(id);
+  const userExists = await User.findOne({ email });
 
-    if(userExists) {
-        return res.status(422).send({
-            msg: "Já existe um usuário com esse email"
-        });
-    }
-    
-    const checkPassword = await bcrypt.compare(password, user.password);
+  if (userExists) {
+    return res.status(400).send({
+      error: 'This e-mail is already in use',
+    });
+  }
 
-    if(!checkPassword) {
-        return res.status(401).send({
-            msg: "Senha incorreta"
-        });
-    }
+  await User.updateOne({ id }, {
+    email,
+    name,
+  });
 
-    try {
-        await User.updateOne({ id }, { email, name });
-
-        res.status(200).send({
-            msg: "Usuário alterado com sucesso",
-            oldData: {
-                name: user.name,
-                email: user.email
-            },
-            newData: {
-                name,
-                email
-            }
-        });
-    } catch (err) {
-        console.log('erro: '+err);
-    
-        res.status(500).send({
-            msg: "Houve um erro no servidor, tente novamente mais tarde"
-        });
-    }
-}
+  res.status(200).send({
+    name,
+    email,
+  });
+};
 
 const editUserPass = async (req, res) => {
-    const { email, password, newPassword } = req.body;
+  const { id } = req.credentials;
+  const { password, confirmPassword } = req.body;
 
-    const user = await User.findOne({ email });
-    const checkPassword = await bcrypt.compare(password, user.password);
+  const user = await User.findById(id);
 
-    if(!checkPassword) {
-        return res.status(401).send({
-            msg: "Senha incorreta"
-        });
-    }
+  if (password !== confirmPassword) {
+    return res.status(400).send({
+      error: 'Passwords do not match',
+    });
+  }
 
-    const salt = await bcrypt.genSalt(12);
-    const passwordHash = await bcrypt.hash(newPassword, salt);
+  const salt = await bcrypt.genSalt(12);
+  const passwordHash = await bcrypt.hash(password, salt);
 
-    try {
-        await User.updateOne({ _id: user._id }, { password: passwordHash });
+  await User.updateOne({ _id: user._id }, { password: passwordHash });
 
-        res.status(200).send({
-            msg: "Senha alterada com sucesso"
-        });
-    } catch (err) {
-        console.log('erro: '+err);
-    
-        res.status(500).send({
-            msg: "Houve um erro no servidor, tente novamente mais tarde"
-        });
-    }
-}
+  res.sendStatus(204);
+};
 
-const makeAdmin = async (req, res) => {
-    const { id, admin } = req.params;
-    const user = await User.findById(id);
+const editUserAdmin = async (req, res) => {
+  const { id } = req.params;
+  const user = await User.findById(id);
 
-    if(!user) {
-        return res.staus(404).send({
-            msg: "Usuário não encontrado"
-        });
-    }
+  await User.updateOne({ _id: user._id }, { admin: !user.admin });
 
-    try {
-        if(admin === "true") {
-            user.admin = true;
-        } else if (admin === "false") {
-            user.admin = false;
-        }
-
-        await user.save();
-
-        res.status(200).send({
-            data: {
-                id: user._id,
-                name: user.name,
-                admin: user.admin
-            }
-        });
-    } catch (err) {
-        console.log('erro: '+err);
-    
-        res.status(500).send({
-            msg: "Houve um erro no servidor, tente novamente mais tarde"
-        });
-    }
-}
+  res.sendStatus(204);
+};
 
 const deleteUser = async (req, res) => {
-    const user = await User.findById(req.params.id);
+  const { id } = req.credentials;
+  const user = await User.findById(id);
 
-    if(!user) {
-        return res.status(404).send({
-            msg: "Usuário não encontrado"
-        });
-    }
+  if (!user) {
+    return res.status(404).send({
+      error: 'User not found',
+    });
+  }
 
-    const checkPassword = await bcrypt.compare(req.body.password, user.password);
+  await User.deleteOne({ _id: user._id });
 
-    if(!checkPassword) {
-        return res.status(401).send({
-            msg: "Senha incorreta"
-        });
-    }
-
-    try {
-        await User.deleteOne({_id: user._id});
-
-        res.status(200).send({
-            msg: "Usuário apagado com sucesso",
-            data: {
-                id: user._id,
-                name: user.name,
-                email: user.email
-            }
-        });
-    } catch (err) {
-        console.log('erro: '+err);
-    
-        res.status(500).send({
-            msg: "Houve um erro no servidor, tente novamente mais tarde"
-        });
-    }
-}
+  res.sendStatus(204);
+};
 
 module.exports = {
-    newUser,
-    loginUser,
-    editUser,
-    editUserPass,
-    makeAdmin,
-    deleteUser
-}
+  createUser,
+  loginUser,
+  editUser,
+  editUserPass,
+  editUserAdmin,
+  deleteUser,
+};
