3790d6ca402b829c9de50f749a661c18ea2b45db
eduardosdl <97840495+eduardosdl@users.noreply.github.com>
2022-11-23 16:50:03 -0300

    update server code to connect mongo db and handle errors

--- src/server.js
+++ src/server.js
@@ -6,24 +6,28 @@ const cors = require('./middlewares/cors');
 const router = require('./router');
 const error = require('./middlewares/errorHandler');
 
-const app = express();
-
-// config
-const port = process.env.PORT || 3000;
-// express
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors);
 // mongoose
 mongoose.Promise = global.Promise;
 mongoose.connect(`mongodb://${process.env.DB_USER}:${process.env.DB_PASS}@localhost:27017/library?authSource=admin`, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 }).then(() => {
-  console.log('conectou no banco');
-}).catch((err) => console.log(`err${err}`));
-// routes
-app.use(router);
-app.use(error);
+  const app = express();
+  const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`Server rodando em http://localhost:${port}`));
+  // parser
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
+  // cors
+  app.use(cors);
+  // router
+  app.use(router);
+  // handle error
+  app.use(error);
+
+  app.listen(port, () => {
+    console.log(`Server rodando em http://localhost:${port}`);
+  });
+}).catch((err) => {
+  console.log(`error: ${err}`);
+});
